0b84bf511677101a209efaa09db0bd3976db5046
Will Hemsley <william.hemsley8@gmail.com>
2021-08-08 20:45:17 +0100

    v1 create a bonding cell that bonded agents keeping cells together

--- src/physical_bond.h
+++ src/physical_bond.h
@@ -7,42 +7,74 @@
 
 namespace bdm {
 
-class CreatePhysicalBond : public PhysicalBond {
-  BDM_BOND_HEADER(PhysicalBond, PhysicalBond, 1);
+/**
+ * This class represents a physical bond between two agents.
+ *
+ * @author willhemsley
+ *
+*/
 
-public:
- CreatePhysicalBond() {}
- explicit CreatePhysicalBond(Agent* agent_a, Agent* agent_b)
-                      : agent_a_(agent_a), agent_b_(agent_b) {}
+class PhysicalBond {
 
- virtual ~CreatePhysicalBond() {}
+  public:
+    PhysicalBond() {}
+
+    explicit PhysicalBond(Agent* agent_a, Agent* agent_b)
+                                : agent_a_(agent_a), agent_b_(agent_b) {}
+
+    virtual ~PhysicalBond() {}
 
 
- // Get and set Agent First Agent (a)
- void SetFirstAgent(Agent* agent_a) {agent_a_ = agent_a; }
- Agent* GetFirstAgent() { return agent_a_; }
+    // Get and set Agent First Agent (a)
+    void SetFirstAgent(Agent* agent_a) {agent_a_ = agent_a; }
+    Agent* GetFirstAgent() { return agent_a_; }
 
- // Get and set Agent Second Agent (b)
- void SetSecondAgent(Agent* agent_b) { agent_b_ = agent_b; }
- Agent* GetSecondAgent() { return agent_b_; }
+    // Get and set Agent Second Agent (b)
+    void SetSecondAgent(Agent* agent_b) { agent_b_ = agent_b; }
+    Agent* GetSecondAgent() { return agent_b_; }
 
- // Set if bond has an effect on first agent (a)
- void setHasEffectOnA(bool hasEffectOnA) { hasEffectOnA_ = hasEffectOnA; }
- bool isHasEffectOnA() { return hasEffectOnA_; }
+    // Set if bond has an effect on first agent (a)
+    void setHasEffectOnA(bool hasEffectOnA) { hasEffectOnA_ = hasEffectOnA; }
+    bool isHasEffectOnA() { return hasEffectOnA_; }
 
- // Set if bond has an effect on second agent (b)
- void setHasEffectOnB(bool hasEffectOnB) { hasEffectOnB_ = hasEffectOnB; }
- bool isHasEffectOnB() { return hasEffectOnB_; }
+    // Set if bond has an effect on second agent (b)
+    void setHasEffectOnB(bool hasEffectOnB) { hasEffectOnB_ = hasEffectOnB; }
+    bool isHasEffectOnB() { return hasEffectOnB_; }
 
- void Run(Agent* agent_a, Agent* agent_b) {
-   ??? Need to actually create the bond behavior here ???
- }
+    void CreateBond(Agent* agent_a, Agent* agent_b) {
+      //??? Lock the two agents together with the bond ???
+      //??? Could a cell be made between the two that excretes a chemical ???
+      //??? Chemotaxis pulls the cells together ???
+      auto* sim = Simulation::GetActive();
+      auto* random = sim->GetRandom();
 
-private:
-  Agent* a_;
-  Agent* b_;
-  bool hasEffectOnA_ = true;
-  bool hasEffectOnB_ = true;
+      // Initialise bond substance => does not diffuse or decay
+      ModelInitializer::DefineSubstance(kBond, "Bond", 0.1, 0.1, 2);
+
+      // find positions of agents
+      const auto& position_a = agent_a->GetPosition(); // get position of agent_1
+      const auto& position_b = agent_b->GetPosition(); // get position of agent_2
+
+      // find midpoint of agents
+      std::vector<Double3> midpoint_pos = (position1 + position2) / 2;
+      auto avg_dist = midpoint_pos[0] + midpoint_pos[1] + midpoint_pos[2] / 3;
+
+      // create new cell (bonding cell) at midpoint
+      MyCell* bond = bond MyCell(midpoint_pos);
+      bond->SetDiameter(avg_dist); // set size of bond to max distance away
+      // bonding cell secretes substabce to keep together
+      bond->AddBehavior(new Secretion("Bond"));
+
+      // 5. agents are attracted to the bond cell chemical
+      agent1->AddBehavior(new Chemotaxis("Bond", 1));
+      agent2->AddBehavior(new Chemotaxis("Bond", 1));
+    }
+
+  private:
+    Agent* agent_a_;
+    Agent* agent_b_;
+    bool hasEffectOnA_ = true;
+    bool hasEffectOnB_ = true;
 
 };
 
